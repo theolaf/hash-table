37175a25eed766e8e9b725e0e56cb91e6c4751c4
theolaf <theo.lafforgue@gmail.com>
2025-07-26 00:43:32 +0200

    added search/insert/delete function signatures

--- src/hash_table.h
+++ src/hash_table.h
@@ -9,6 +9,9 @@ typedef struct {
   kv_pair** items;
 } hash_table;
 
-hash_table* ht_new();
+hash_table* new_hash_table();
+void delete_hash_table(hash_table* ht);
 
-void delete_ht_hash_table(hash_table* ht);
+void insert_kv_pair(hash_table* ht, const char* key, const char* value);
+char* search_kv_pair(hash_table* ht, const char* key);
+void delete_kv_pair(hash_table* ht, const char* key);
