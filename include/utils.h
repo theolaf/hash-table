d2a173cc9f94d3af0b1e87c125f96870102017b2
theolaf <theo.lafforgue@gmail.com>
2025-07-30 20:26:03 +0200

    implemented prime util functions

--- include/utils.h
+++ include/utils.h
@@ -1,3 +1,5 @@
 #pragma once
 
 int get_hash(const char *key, const int num_buckets, const int retries);
+
+int next_prime(int x);
--- src/utils.c
+++ src/utils.c
@@ -1,6 +1,7 @@
 #include "utils.h"
 
 #include <math.h>
+#include <stdbool.h>
 #include <string.h>
 
 static int hash(const char *string, const int prime, const int num_buckets)
@@ -21,4 +22,33 @@ int get_hash(const char *key, const int num_buckets, const int retries)
     const int hashed_a = hash(key, 151, num_buckets);
     const int hashed_b = hash(key, 163, num_buckets);
     return (hashed_a + (retries * (hashed_b + 1))) % num_buckets;
+}
+
+static bool is_prime(const int x)
+{
+    if (x < 4)
+    {
+        return true; // untrue mathematically but it works for our purpose here
+    }
+    if ((x % 2) == 0)
+    {
+        return false;
+    }
+    for (int i = 3; i <= floor(sqrt((double)x)); i += 2)
+    {
+        if ((x % i) == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int next_prime(int x)
+{
+    while (is_prime(x) != 1)
+    {
+        x++;
+    }
+    return x;
 }
