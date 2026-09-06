deca2eb374183cc4c3d7af1b6b6ee04794d2b026
Theo Lafforgue <theo.lafforgue@gmail.com>
2026-09-06 18:04:31 +0200

    fixed prime status for small ints

--- src/utils.c
+++ src/utils.c
@@ -29,14 +29,19 @@ int get_hash(const char *key, const int num_buckets, const int retries)
 
 static bool is_prime(const int x)
 {
-    if (x < 4)
-    {
-        return true; // untrue mathematically but it works for our purpose here
+    // Handle edge cases: 0 and 1 are not prime, 2 and 3 are prime
+    if (x < 2) {
+        return false;
     }
+    if (x == 2 || x == 3) {
+        return true;
+    }
+    // Even numbers > 2 are not prime
     if ((x % 2) == 0)
     {
         return false;
     }
+    // Check divisibility by odd numbers up to sqrt(x)
     for (int i = 3; i <= floor(sqrt((double)x)); i += 2)
     {
         if ((x % i) == 0)
